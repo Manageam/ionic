0952186a1b65098fc5b3c06db8de0bd9e5978de9
uduakabaci <uduakabaciudofe@gmail.com>
2021-11-22 12:03:34 +0100

    update the user service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -6,16 +6,42 @@ import { AuthenticationService } from '../authentication/authentication.service'
   providedIn: 'root',
 })
 export class UserService {
-  url = environment.apiUrl + 'user';
+  url = environment.apiUrl + 'users';
   user: any = {};
   constructor(private http: HttpClient, private auth: AuthenticationService) {}
 
-  getInfo() {
+  getDetails() {
     const user = this.auth.loggedUser();
-    return this.http.get(`${this.url}/${user.id}/info`);
+    return this.http.get(`${this.url}/details/${user.id}`);
   }
-  updateInfo(data) {
+
+  getPhoto() {
     const user = this.auth.loggedUser();
-    return this.http.put(`${this.url}/${user.id}/profile`, data);
+    return this.http.get(`${this.url}/userPhoto/${user.id}`);
+  }
+
+  updateDetails(data) {
+    const user = this.auth.loggedUser();
+    return this.http.put(`${this.url}/details/${user.id}`, data);
+  }
+
+  login(data) {
+    return this.http.post(`${this.url}/login`, data);
+  }
+
+  register(data) {
+    return this.http.post(`${this.url}/register`, data);
+  }
+
+  updatePhoto(id, data) {
+    return this.http.post(`${this.url}/photo/${id}`, data);
+  }
+
+  resetPassword(data) {
+    return this.http.post(`${this.url}/generateresetpassword`, data);
+  }
+
+  changePassword(id, data) {
+    return this.http.post(`${this.url}/password/${id}`, data);
   }
 }
