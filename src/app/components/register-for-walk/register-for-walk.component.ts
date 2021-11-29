351a62f6cda35c25ba75532c116f70b29aad954d
uduakabaci <uduakabaciudofe@gmail.com>
2021-11-29 12:40:55 +0100

    fix reminder api

--- src/app/components/register-for-walk/register-for-walk.component.ts
+++ src/app/components/register-for-walk/register-for-walk.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { GlobalService } from 'src/app/services/global/global.service';
+import { WalkService } from 'src/app/services/walk/walk.service';
 
 @Component({
   selector: 'app-register-for-walk',
@@ -8,10 +10,23 @@ import { ModalController } from '@ionic/angular';
 })
 export class RegisterForWalkComponent implements OnInit {
   walk: any = {};
-  constructor(public modalController: ModalController) {}
+  constructor(
+    public modalController: ModalController,
+    private walkService: WalkService,
+    private global: GlobalService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.walkService.get().subscribe((data) => (this.walk = data));
+  }
   save() {
-    console.log('save');
+    console.log(this.walk);
+    this.walkService.register(this.walk);
+    this.modalController.dismiss();
+    this.global.alert(
+      'Diabetes Walk',
+      "Email address successfully registered. We'll notify you when the event timeline is ready",
+      ['Okay']
+    );
   }
 }
