9e0c662987bcade0adf3500e3cbeee99874ca538
uduakabaci <uduakabaciudofe@gmail.com>
2021-12-01 20:53:48 +0100

    update colors

--- src/app/pages/logged/home/home.page.ts
+++ src/app/pages/logged/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { SearchComponent } from 'src/app/components/search/search.component';
 import { UserService } from 'src/app/services/user/user.service';
@@ -13,7 +14,8 @@ export class HomePage implements OnInit {
   tip: any = {};
   constructor(
     private userService: UserService,
-    private modalController: ModalController
+    private modalController: ModalController,
+    public router: Router
   ) {}
 
   ngOnInit() {
