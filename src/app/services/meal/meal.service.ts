5c44932d00170ae6e6c3b5c93c0d1000ffd8d9a9
uduakabaci udofe <uduakabaciudofe@gmail.com>
2022-02-01 12:56:56 +0100

    switch the environment file to dev

--- src/app/services/meal/meal.service.ts
+++ src/app/services/meal/meal.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { UserService } from '../user/user.service';
 @Injectable({
   providedIn: 'root',
