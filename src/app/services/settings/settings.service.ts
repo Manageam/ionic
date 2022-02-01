3e1ca9c883d5860bc1b24ac04c93c05d8f4a0008
uduakabaci udofe <uduakabaciudofe@gmail.com>
2022-02-01 12:59:02 +0100

    add fcm token url

--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UserService } from '../user/user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SettingsService {
+  constructor(private http: HttpClient, private userService: UserService) {}
 
-  constructor() { }
+  pushFCMToken(token) {
+    const user = this.userService.fetchDetails();
+    const {
+      user_details: { id },
+    } = user;
+    this.http.post(`${environment.apiUrl}/fcm-token`, {
+      fcm_token: token,
+      user_id: id,
+    });
+  }
 }
