b7dce41344f3527e5a5cd9db301ad8ec57308610
uduakabaci udofe <uduakabaciudofe@gmail.com>
2022-02-24 15:46:24 +0100

    stop logged in user from goin to auth page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthService } from './services/auth/auth.service';
+import { GuestService } from './services/guest/guest.service';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [GuestService],
   },
   {
     path: '',
